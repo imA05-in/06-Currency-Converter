05bb075b50c8f20e2e710ffc1d9de8f8fba178ce
imA05-in <abhishek29112005@gmail.com>
2025-08-03 18:48:56 +0530

    added functionality to InputBox.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
-import './App.css'
-import InputBox from './components/InputBox';
+import './App.css';
+import {InputBox} from './components/index.js';
 
 function App() {
 
@@ -11,15 +11,22 @@ function App() {
             backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
         }}
     >
-        <div className="w-full ">
-            <div className="w-full h-80 max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
-                <form>
+        <div className="w-full">
+            <div className="w-full  max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                <form className=''>
+                    
+                    <InputBox label = "From"
+                    />
+                    
+                    
+                    
                     <button
                             type="button"
                             className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
-                        >
+                            >
                             swap
                         </button>
+                    <InputBox label="To"/>
             
                     <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
                         Convert 
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,17 +1,52 @@
 
 function InputBox({
-    label, 
     className = "",
-}){
-      return (
-        <></>
-    );
+    label ,
+    amount ,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd"
+}
+    
+) {
+    return (
+        <div className={`bg-white rounded-lg text-sm p-3 flex ${className} my-3`}>
+
+            <div className="left w-1/2 pl-2 flex flex-col justify-around">
+                <label htmlFor="" className="text-black/40">
+                {label}
+                </label>
+                <input 
+                type="number"
+                className="outline-none w-full py-1.5 "
+                placeholder="Amount"
+                value={amount}
+                onChange={(e)=> onAmountChange && onAmountChange(Number(e.target.value))}
+                />
+                
+            </div>
+
+            <div className="w-1/2 flex flex-wrap items-end justify-around pr-2 flex-col">
+                <p className="text-black/40 mb-2 ">Currency Type</p>
+                <select
+                    name=""
+                    id=""
+                    className="rounded-lg px-1 bg-gray-100 cursor-pointer outline-none"
+                    value={selectCurrency}
+                    onChange={(e)=> onCurrencyChange && onCurrencyChange(e.target.value)}
+                >
+                    
+                   {currencyOptions.map((currency)=> (
+                    <option key={currency} value={currency}>{currency}</option>
+                    ))} 
+
+                </select>
+            </div>
+
+        </div>
+    )
 
 }
 
-const dd = [dw2,ew]
-
-dd.map((i)=>console.log(i))
-
 export default InputBox
-
